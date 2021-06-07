5943778a5639979aab102784f2e91105491be32f
AlejandroGonzalez02 <agonzalezalonso@danielcastelao.org>
2021-06-07 09:27:06 +0200

    Commit exercicio2

---
+++ src/main/kotlin/Exercicio2.kt
@@ -0,0 +1,5 @@
+class Exercicio2(size: Int, relleno: (Int) -> Int) {
+
+    var aleatorios = IntArray(size,relleno)
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
 fun main(args: Array<String>) {
 
+//Apartado1
+    var contador = 0
+    for (resultado in Exercicio1(11).tabla7){
+        println("7 x $contador = $resultado")
+        contador++
+
+    }
 }
